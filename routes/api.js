184fcd99f30fc396b7cfb95f4e4e8fa2254939a8
ArturoRey89 <arturoreyes89@gmail.com>
2022-12-02 19:44:17 +0100

    fix: database changed

--- routes/api.js
+++ routes/api.js
@@ -4,28 +4,26 @@ const mongoose = require('mongoose');
 
 //connect to database 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.log("Database connection error: ", error));
 db.on("connected", () => console.log("Connected to database"));
 
 //database structure
 const issueSchema = new mongoose.Schema({
+  project_name: {type: String, required: true},
+  assigned_to: { type: String, default: "" },
+  status_text: { type: String, default: "" },
+  open: { type: Boolean, default: true },
   issue_title: {type: String, required: true },
   issue_text: {type: String, required: true },
-  created_on: { type: String, default: Date() },
-  updated_on: { type: String, default: Date() },
   created_by: {type: String, required: true },
-  assigned_to: { type: String, default: "" },
-  open: { type: Boolean, default: true },
-  status_text: { type: String, default: "" }
-});
-const projectSchema = new mongoose.Schema({
-  project_name: String,
-  project_issue_tracker: [issueSchema]
+  created_on: { type: String, default: Date() },
+  updated_on: { type: String, default: Date() }
 });
 
-const Project = mongoose.model("Project", projectSchema);
 const Issue = mongoose.model("Issue", issueSchema);
+const ObjectId = mongoose.Types.ObjectId;
 
 module.exports = function (app) {
 
@@ -67,34 +65,3 @@ module.exports = function (app) {
     });
     
 };
-
-
-const findProjectByName = (projectName, done) => {
-  Project.findOne( {name: projectName}, (err, projectFound) => {
-      if (err) console.log("findOne error",err);
-      done(null, projectFound);
-    }
-  );
-};
-
-const findProjectAndAddIssue = (projectName, issue, done) => {
-  if(issue.issue_title != 'test1') {//--------ONLY FOR Production
-          findProjectByName(projectName, (err, project) => {
-            //__append to project__
-            if(project.issue_array){
-              project.issue_array.push(issue)
-              project.save((err) =>  err)
-            }
-            //__create project__
-            if(project == false){
-              project = new Project({name: projectName,
-                                            issue_array: [issue]})
-              project.save((err) => err)
-            }
-            done(null, project)
-          })
-        }
-  else{
-    done(`error: Project (${projectName}) not found`,null)
-  }
-}
